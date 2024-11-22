f0a3847a2553375e2a10155b6031d1aadf9c244a
rafly <raflyalkhairiii@gmail.com>
2024-11-22 16:26:52 +0700

    menambahkan model dan controller laporan belum dengan fungsinya

---
+++ app/Http/Controllers/LaporanPenerimaanController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LaporanPenerimaan;
+
+class LaporanPenerimaanController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LaporanPenyaluran.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LaporanPenyaluran;
+
+class LaporanPenyaluranController
+{
+    public function index()
+    {
+        //
+    }
+}
